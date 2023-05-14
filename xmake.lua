672a62f70372d586b1d0f1f1bdd7c83ba7a2fea0
zuoxinyu <iamzuoxinyu@gmail.com>
2023-05-14 13:49:45 +0800

    linux use vcpkg

--- xmake.lua
+++ xmake.lua
@@ -1,10 +1,6 @@
 set_project('webrtc-rdp')
 add_rules('mode.debug', 'mode.release')
-if is_os('windows') then
-    set_toolchains('clang')
-else
-    set_toolchains('clang', 'yasm')
-end
+set_toolchains('clang')
 set_defaultmode("debug")
 
 local webrtc_dir = '../webrtc'
@@ -32,15 +28,15 @@ local webrtc_obj_dir = path.join(webrtc_src_dir, webrtc_out_dir, 'obj')
 
 local gn_args = {
     'is_debug=true',
-    'enable_libaom=true',      -- av1 support
-    'rtc_use_h264=false',      --  use custom h264 impl
+    'enable_libaom=true', -- av1 support
+    'rtc_use_h264=false', --  use custom h264 impl
     'rtc_include_tests=false',
     'rtc_enable_protobuf=false',
     -- 'ffmpeg_branding=\"Chrome\"',
     'use_rtti=true',           -- typeinfo
     'use_custom_libcxx=false', -- stdlib
     'use_debug_fission=true',  -- -gsplit-dwarf
-    'rtc_enable_symbol_export=true',
+    -- 'rtc_enable_symbol_export=true',
 }
 
 if is_os('windows') then
@@ -52,8 +48,8 @@ if is_os('windows') then
         'rtc_include_tests=false',
         'rtc_enable_protobuf=false',
         -- for windows
-        'use_lld=false', -- linker
-        'use_rtti=true', -- typeinfo
+        'use_lld=false',           -- linker
+        'use_rtti=true',           -- typeinfo
         'use_custom_libcxx=false', -- stdlib
         'fatal_linker_warnings=false',
         'treat_warnings_as_errors=false',
@@ -67,52 +63,60 @@ local gn_cmd = string.format([[gn gen %s --args="%s"]], webrtc_out_dir, table.co
 local ninja_cmd = string.format([[ninja -C %s]], webrtc_out_dir)
 local mt_cmd = [[mt.exe -manifest .\dezk.manifest -outputresource:'.\build\windows\x64\debug\dezk.exe;#1']]
 
-function windows_options()
+local function windows_options()
     -- clang windows workarounds
-    -- add_cxxflags('-fms-runtime-lib=static_dbg', '-fms-compatibility')
-    add_cxxflags('-fms-extensions')
-    add_defines('SDL_MAIN_HANDLED')
-    -- boost workarounds
+    add_cxxflags('-fms-runtime-lib=static_dbg', '-fms-extensions')
     add_defines(
+        'SDL_MAIN_HANDLED',
         'BOOST_ASIO_HAS_STD_COROUTINE',
         'BOOST_ASIO_HAS_CO_AWAIT',
         'BOOST_JSON_NO_LIB',
-        'BOOST_URL_NO_LIB')
-    add_syslinks('kernel32', 'user32', 'gdi32', 'winspool', 'comdlg32', 'advapi32', 'shell32', 
+        'BOOST_URL_NO_LIB',
+        'FMT_HEADER_ONLY')
+    add_syslinks('kernel32', 'user32', 'gdi32', 'winspool', 'comdlg32', 'advapi32', 'shell32',
         'ole32', 'oleaut32', 'uuid', 'secur32', 'strmiids', 'mfuuid', 'd3d11', 'wmcodecdspuuid', 'dmoguids',
         'odbc32', 'odbccp32', 'version', 'winmm', 'setupapi', 'imm32', 'iphlpapi', 'msdmo', 'shcore', 'dwmapi', 'dxgi')
 end
 
-if is_os('linux') then
-    add_requires('spdlog', 'fmt')
-    add_requires('boost_json', 'boost_url')
-    add_requires('SDL2', 'SDL2_ttf', 'glew')
-    add_requires('avutil', 'avcodec', 'avformat')
-elseif is_os('windows') then
-    add_requires('abseil', {system = true})
-    add_requires('boost-asio', {alias = 'boost_asio', system = true, debug=true})
-    add_requires('boost-beast', {alias = 'boost_beast', system = true, debug=true})
-    add_requires('boost-json', {alias = 'boost_json', system = true, debug=true})
-    add_requires('boost-url', {alias = 'boost_url', system = true, debug=true})
-    add_requires('glew', {alias = 'glew', system = true})
-    add_requires('opengl', {alias = 'GL', system = true})
-    add_requires('sdl2', {alias = 'SDL2', system = true})
-    add_requires('sdl2-ttf', {alias = 'SDL2_ttf', system = true})
-    add_requires('freetype', {alias = 'freetype', system = true})
-    add_requires('spdlog', {alias = 'spdlog', system = true, })
-    add_requires('fmt', {alias = 'fmt', system = true, debug=true})
-    add_requires('nlohmann-json', {alias = 'nlohmann-json', system = true})
-    add_requires('libyuv', {alias = 'libyuv', system = true})
-    add_requires('ffmpeg[avcodec]', {alias = 'avcodec', system = true})
-    add_requires('ffmpeg[avutil]', {alias = 'avutil', system = true})
-    add_requires('ffmpeg[avformat]', {alias = 'avformat', system = true})
-    add_requires('x264', {alias = 'x264', system = true})
-    add_requires('zlib', {alias = 'zlib', system = true})
-    add_requires('libpng', {alias = 'libpng', system = true})
-    add_requires('bzip2', {alias = 'bzip2', system = true})
-    add_requires('brotli', {alias = 'brotli', system = true})
-    add_requires('liblzma', {alias = 'liblzma', system = true})
+local function linux_options()
+    add_links('glib-2.0', 'gobject-2.0', 'gio-2.0', 'gbm')
+    add_links('X11', 'Xext', 'Xfixes', 'Xdamage', 'Xrandr', 'Xcomposite', 'Xtst')
+    add_links('rt', 'drm')
 end
+
+if is_os('linux') then
+    add_requires('xdo', { system = true })
+end
+
+add_requires('boost-asio', { system = true, debug = true })
+add_requires('boost-beast', { system = true, debug = true })
+add_requires('boost-json', { system = true, debug = true })
+add_requires('boost-url', { system = true, debug = true })
+add_requires('abseil', { system = true })
+-- glew
+add_requires('glew', { system = true })
+add_requires('opengl', { system = true })
+-- end glew
+add_requires('sdl2', { system = true })
+-- sdl2-ttf
+add_requires('sdl2-ttf', { system = true })
+add_requires('freetype', { system = true })
+add_requires('bzip2', { system = true })
+add_requires('brotli', { system = true })
+add_requires('zlib', { system = true })
+add_requires('libpng', { system = true })
+-- end sdl2-ttf
+add_requires('spdlog', { system = true, })
+add_requires('fmt', { system = true, debug = true })
+add_requires('nlohmann-json', { system = true })
+add_requires('libyuv', { system = true })
+-- ffmpeg
+add_requires('ffmpeg[avcodec]', { alias = 'avcodec', system = true })
+add_requires('ffmpeg[avutil]', { alias = 'avutil', system = true })
+add_requires('ffmpeg[avformat]', { alias = 'avformat', system = true })
+add_requires('x264', { system = true })
+add_requires('liblzma', { system = true })
+-- end ffmpeg
 
 target('dezk', function()
     set_default(true)
@@ -126,23 +130,20 @@ target('dezk', function()
     add_linkdirs(webrtc_obj_dir)
     add_links('webrtc')
 
+    add_cxxflags('-Wno-deprecated-declarations')
+
     if is_os('linux') then
-        add_defines('WEBRTC_POSIX', 'WEBRTC_LINUX', 'WEBRTC_USE_X11', 'RTC_DISABLE_LOGGING')
-        add_cxxflags('-Wno-deprecated-declarations')
-        add_packages('boost_json', 'boost_url', 'boost_beast', 'spdlog', 'SDL2')
-        add_syslinks('avcodec', 'avutil', 'avformat')
-        add_syslinks('dbus-1', 'glib-2.0', 'gobject-2.0', 'gmodule-2.0', 'gio-2.0', 'gbm')
-        add_syslinks('xdo', 'xcb', 'X11', 'Xext', 'Xfixes', 'Xdamage', 'Xrandr', 'Xrender', 'Xau', 'Xdmcp', 'Xcomposite',
-            'Xtst')
-        add_syslinks('rt', 'atomic', 'GL', 'GLEW', 'drm', 'SDL2_ttf')
+        add_defines('WEBRTC_POSIX', 'WEBRTC_LINUX', 'WEBRTC_USE_X11')
+        add_packages('xdo')
+        linux_options()
     elseif is_os('windows') then
-        add_defines('WEBRTC_WIN', 'NOMINMAX', '_WIN32_WINNT=0x0601', '_CRT_SECURE_NO_WARNINGS', 'FMT_HEADER_ONLY')
-        add_includedirs(webrtc_third_party_dirs)
-        add_packages('glew', 'spdlog', 'SDL2', 'SDL2_ttf', 'nlohmann-json', 'GL', 'freetype', 'zlib', 'liblzma', 'brotli', 'libpng', 'bzip2')
-        add_packages('avcodec', 'avutil', 'avformat', 'x264')
-        add_packages('boost_asio', 'boost_json', 'boost_url', 'abseil')
+        add_defines('WEBRTC_WIN', 'NOMINMAX', '_WIN32_WINNT=0x0601', '_CRT_SECURE_NO_WARNINGS', 'SDL_MAIN_HANDLED')
         windows_options()
     end
+    add_packages('boost-json', 'boost-url', 'spdlog', 'abseil', 'nlohmann-json')
+    add_packages('sdl2', 'sdl2-ttf', 'glew')
+    add_packages('avcodec', 'avutil', 'avformat', 'libyuv')
+    add_packages('opengl', 'freetype', 'zlib', 'liblzma', 'brotli', 'libpng', 'bzip2', 'x264')
 
     if is_os('linux') then
         after_build(function(target)
@@ -159,14 +160,10 @@ target('signal_server', function()
 
     add_packages('spdlog', 'fmt')
     if is_os('windows') then
-        add_cxxflags('-fms-runtime-lib=static_dbg')
-        -- add_cxxflags('/bigobj')
         windows_options()
-        add_packages('boost_asio', 'boost_json', 'boost_url', 'boost_beast')
         add_defines('NOMINMAX', '_WIN32_WINNT=0x0601', '_CRT_SECURE_NO_WARNINGS')
-    else
-        add_packages('boost_json', 'boost_url', 'boost_beast', { configs = { shared = false } })
     end
+    add_packages('boost-asio', 'boost-json', 'boost-url', 'boost-beast')
 
     if is_os('linux') then
         after_build(function(target)
@@ -181,7 +178,7 @@ target('video_player_test', function()
     add_files('src/client/sink/*.cc')
     add_includedirs('src', webrtc_src_dir)
     add_includedirs(webrtc_third_party_dirs)
-    add_packages('spdlog', 'fmt', 'SDL2', 'SDL2_ttf', 'glew', 'GL')
+    add_packages('spdlog', 'fmt', 'sdl2', 'sdl2-ttf', 'glew', 'opengl')
     add_linkdirs(webrtc_obj_dir)
     add_links('webrtc')
     if is_os('windows') then
@@ -221,6 +218,7 @@ task('echo-cmd', function()
         cprint('${yellow}webrtc branch${clear}: %s', check_cmd)
         cprint('${yellow}webrtc config cmd${clear}: %s', gn_cmd)
         cprint('${yellow}webrtc build cmd${clear}: %s', ninja_cmd)
+        cprint('${yellow}mt cmd${clear}: %s', mt_cmd)
     end)
     set_menu {
         usage = 'xmake echo-cmd',
